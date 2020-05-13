9dfede248e484dde862045cfbe9cb40c40da1b2c
worbs106 <worbs106@nate.com>
2020-05-13 15:54:22 +0900

    이름변경

--- myfirst1-app.js
+++ myfirst1-app.js
@@ -6,6 +6,7 @@ const server = http.createServer((req, res) => {
     res.statusCode = 200;
     res.setHeader('Content-Type', 'text/plain');
     res.end(' World');
+  
   });
   
   server.listen(port, hostname, () => {
